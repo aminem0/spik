64577c97da680ffd4136b55d72e50015855af09a
El-Amine Mimouni <el-amine.mimouni@mcgill.ca>
2025-11-14 11:22:49 -0500

    try more mixs

--- lua/sco/sources/mixs.lua
+++ lua/sco/sources/mixs.lua
@@ -30,4 +30,27 @@ return {
 ]],
         },
     },
+    {
+        label = "mixs:samp_collect_device",
+        insertText = "mixs:0000002",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "mixs:samp_collect_device",
+        deprecated = false,
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [https://w3id.org/mixs/0000002]
+
++ **Type**
+- [rdf:Property]
+
++ **Range**
+- [xsd:string]
+
++ **Definition**
+- The device used tocollect an environmental sample. This field accepts terms listed under environmental sampling device ([http://purl.obolibrary.org/obo/ENVO]). This field also accepts terms listed under specimen collection device ([http://purl.obolibrary.org/obo/GENEPIO_0002094]).
+]],
+        },
+    },
 }
