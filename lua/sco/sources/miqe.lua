c50943c601c6cd801637b8588eaf2e8d03cfcd8d
El-Amine Mimouni <el-amine.mimouni@mcgill.ca>
2025-11-14 17:16:31 -0500

    finished miqe: namespace

--- lua/sco/sources/miqe.lua
+++ lua/sco/sources/miqe.lua
@@ -2,6 +2,62 @@ local cmp = require("cmp")
 
 return {
     {
+        label = "miqe:amplificationReactionVolume",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:amplificationReactionVolume",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/amplificationReactionVolume]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:decimal]
+
++ **Definition**
+- PCR reaction volume.
+
++ **Examples**
+- `22`
+]],
+        },
+    },
+    {
+        label = "miqe:amplificationReactionVolumeUnit",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:amplificationReactionVolumeUnit",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/amplificationReactionVolumeUnit]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:string]
+
++ **Definition**
+- Unit used for PCR reaction volume. Many of the instruments require preparation of a much larger initial sample volume than is usually analyzed.
+
++ **Examples**
+- `µl`
+]],
+        },
+    },
+    {
         label = "miqe:ampliconSize",
         kind = cmp.lsp.CompletionItemKind.Property,
         description = "miqe:ampliconSize",
@@ -86,6 +142,302 @@ return {
         },
     },
     {
+        label = "miqe:automaticBaselineValue",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:automaticBaselineValue",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/automaticBaselineValue]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:boolean]
+
++ **Definition**
+- Whether the baseline value was set by the instrument or manually.
+
++ **Examples**
+- `true`
+]],
+        },
+    },
+    {
+        label = "miqe:automaticThresholdQuantificationCycle",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:automaticThresholdQuantificationCycle",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/automaticThresholdQuantificationCycle]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:boolean]
+
++ **Definition**
+- Whether the threshold was set by the instrument or manually.
+
++ **Examples**
+- `true`
+]],
+        },
+    },
+    {
+        label = "miqe:baselineValue",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:baselineValue",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/baselineValue]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:integer]
+
++ **Definition**
+- The number of cycles when fluorescence signal from the target amplification is below background fluorescence not originated from the real target amplification.
+
++ **Examples**
+- `15`
+]],
+        },
+    },
+    {
+        label = "miqe:contaminationAssessment",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:contaminationAssessment",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/contaminationAssessment]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:boolean]
+
++ **Definition**
+- Whether DNA or RNA contamination assessment was done or not.
+
++ **Examples**
+- `true`
+]],
+        },
+    },
+    {
+        label = "miqe:estimatedNumberOfCopies",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:estimatedNumberOfCopies",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/estimatedNumberOfCopies]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:integer]
+
++ **Definition**
+- Number of target molecules per µl. Mean copies per partition (?) can be calculated using the number of partitions (n) and the estimated copy number in the total volume of all partitions (m) with a formula ?=m/n.
+
++ **Examples**
+- `10300`
+]],
+        },
+    },
+    {
+        label = "miqe:experimentalVariance",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:experimentalVariance",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/experimentalVariance]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Definition**
+- Multiple biological replicates are encouraged to assess total experimental variation. When single dPCR experiments are performed, a minimal estimate of variance due to counting error alone must be calculated from the binomial (or suitable equivalent) distribution.
+]],
+        },
+    },
+    {
+        label = "miqe:partitionVolume",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:partitionVolume",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/partitionVolume]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:decimal]
+
++ **Definition**
+- An accurate estimation of partition volume. The sum of the partitions multiplied by the partition volume will enable the total volume of the reaction to be calculated.
+
++ **Examples**
+- `1`
+]],
+        },
+    },
+    {
+        label = "miqe:partitionVolumeUnit",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:partitionVolumeUnit",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/partitionVolumeUnit]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:string]
+
++ **Definition**
+- Unit used for partition volume.
+
++ **Examples**
+- `nl`
+]],
+        },
+    },
+    {
+        label = "miqe:pcr_analysis_software",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:pcr_analysis_software",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/pcr_analysis_software]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:string]
+
++ **Definition**
+- The program used to analyze the d(d)PCR runs.
+
++ **Examples**
+- `BIO-RAD QuantaSoft`
+]],
+        },
+    },
+    {
+        label = "miqe:pcr_primer_lod",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:pcr_primer_lod",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/pcr_primer_lod]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Definition**
+- The assay's ability to detect the target at low levels.
+
++ **Examples**
+- `51`
+]],
+        },
+    },
+    {
+        label = "miqe:pcr_primer_loq",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:pcr_primer_loq",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/pcr_primer_loq]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Definition**
+- The assay's ability to quantify copy number at low levels.
+
++ **Examples**
+- `184`
+]],
+        },
+    },
+    {
         label = "miqe:probeQuencher",
         kind = cmp.lsp.CompletionItemKind.Property,
         description = "miqe:probeQuencher",
@@ -138,6 +490,34 @@ return {
 
 + **Examples**
 - `FAM`
+]],
+        },
+    },
+    {
+        label = "miqe:quantificationCycle",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "miqe:quantificationCycle",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [http://rs.gbif.org/terms/miqe/quantificationCycle]
+
++ **Type**
+- [owl:DatatypeProperty]
+- [rdf:Property]
+
++ **Domain**
+- [dwc:MolecularProtocol]
+
++ **Range**
+- [xsd:decimal]
+
++ **Definition**
+- The number of cycles required for the fluorescent signal to cross a given value threshold above the baseline. Quantification cycle (Cq), threshold cycle (Ct), crossing point (Cp), and take-off point (TOP) refer to the same value from the real-time instrument. Use of quantification cycle (Cq), is preferable according to the RDML (Real-Time PCR Data Markup Language) data standard ([http://rdml.org]).
+
++ **Examples**
+- `37.9450950622558`
 ]],
         },
     },
